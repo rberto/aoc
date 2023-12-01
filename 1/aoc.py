847eb84defdd4e205802d1dfcd0892eface9c331
Romain <romain.bertolini@gmail.com>
2023-12-01 14:02:35 -0800

    Part1 complete

---
+++ 1/aoc.py
@@ -0,0 +1,35 @@
+"""
+As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended 
+by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble
+ reading the values on the document.
+
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration 
+value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit 
+and the last digit (in that order) to form a single two-digit number.
+
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+Consider your entire calibration document. What is the sum of all of the calibration values?
+"""
+
+def getfirstdigit(string):
+    for c in string:
+        if c.isdigit():
+            return c
+
+sum = 0
+
+with open("/home/romain/Documents/code/aoc2023/1/input.txt") as f:
+    for line in f.readlines():
+        first = getfirstdigit(line.strip())
+        second = getfirstdigit(reversed(line.strip()))
+        sum += int(f"{first}{second}")
+
+print(sum)
